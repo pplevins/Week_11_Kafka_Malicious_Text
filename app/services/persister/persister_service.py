3411eca82017368b04a2cb52242638826f583318
berale <b.y.berale@gmail.com>
2025-08-28 11:38:46 +0300

    complete persister service

--- app/services/persister/persister_service.py
+++ app/services/persister/persister_service.py
@@ -1,4 +1,7 @@
+from app.core import Database
+from app.dal import TweetsDal
 from app.models import Consumer
+
 
 
 class PersisterService:
@@ -6,6 +9,21 @@ class PersisterService:
         self.topics = ('enriched_preprocessed_tweets_antisemitic','enriched_preprocessed_tweets_not_antisemitic')
         self.topic_conversion = {self.topics[0]:'tweets_antisemitic', self.topics[1]:'tweets_not_antisemitic'}
         self.consumer = Consumer(self.topics)
+        self.dal = TweetsDal(Database())
+
+    def insert_to_db(self):
+
+        for message in self.consumer.get_consumed_messages():
+            self.dal.insert_document(self.topic_conversion[message.topic], message.value)
+
+
+if __name__ == '__main__':
+    PersisterService().insert_to_db()
+
+
+
+
+
 
 
 
